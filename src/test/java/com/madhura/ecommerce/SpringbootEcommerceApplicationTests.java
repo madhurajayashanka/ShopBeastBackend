7b3eb73504399046863c05e3c15fbf5c473bd3e8
madhurajayashanka <madhuraweerasooriye@gmail.com>
2022-07-27 20:13:14 +0530

    Initial commit

---
+++ src/test/java/com/madhura/ecommerce/SpringbootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhura.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
