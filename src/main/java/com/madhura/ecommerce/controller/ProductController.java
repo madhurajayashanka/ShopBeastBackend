8f27018534996593ba495f2bc924627e73d381fd
madhurajayashanka <madhuraweerasooriye@gmail.com>
2022-08-05 19:45:20 +0530

    4

--- src/main/java/com/madhura/ecommerce/controller/ProductController.java
+++ src/main/java/com/madhura/ecommerce/controller/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
-@CrossOrigin("http://localhost:4200")
 public class ProductController {
 
     @Autowired
